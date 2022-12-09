57ff2fb33a6093d56ea3daeb6f51e4eca84f12ad
r-nidhu31 <r.nidhu31@Gmail.com>
2022-12-09 15:48:37 +0000

    finished

--- primes.py
+++ primes.py
@@ -3,4 +3,31 @@
 
 def primes(number_of_primes):
     list = []
+
+    if(number_of_primes<=0):
+        raise ValueError("Only integers greater than 0 are allowed")
+ 
+    numberToCheck = 2
+
+    while number_of_primes:
+        if isPrime(numberToCheck):
+            list.append(numberToCheck)
+            number_of_primes = number_of_primes-1
+        numberToCheck = numberToCheck+1
+    
     return list
+
+def isPrime(numberToCheck):
+    for x in range(2, numberToCheck):
+        if(numberToCheck%x == 0):
+            return False
+    return True
+
+
+
+
+
+
+
+
+
